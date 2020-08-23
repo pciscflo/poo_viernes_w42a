d50954a49bcc2c5a48262775d82b8b8fb49a5bfd
escorpio80@gmx.com <escorpio80@gmx.com>
2020-08-22 19:21:58 -0500

    vx

--- Sesion3/relaciones1.rb
+++ Sesion3/relaciones1.rb
@@ -1,6 +1,6 @@
 =begin
 Una empresa desea calcular el sueldo a pagar a un grupo de trabajadores.
-Los trabajadores pueden ser de dos tipos: Fijos o Contratados. 
+Los trabajadores pueden ser de dos tipos: Asociados o Contratados. 
 En el caso de los Contratados el sueldo se calcula mediante la fórmula:
 Horas Trabajadas * Precio por Hora. 
 En el caso de los Asociados el sueldo debe calcular:
